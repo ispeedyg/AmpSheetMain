e4da4faf0015d0bbf4215e092a3cc33170a5c263
SpeedyG <speedy@ispeedyg.com>
2023-09-26 13:35:52 -0700

    about to try something drastic

--- xlwingsBasic.py
+++ xlwingsBasic.py
@@ -17,14 +17,17 @@ wattsSheet = pd.read_excel(path, 'Watts').to_dict()
 startingPositions = [(3,9,1), (16,22,1), (28,34,1), (41,47,1), (54,60,1), (67,73,1), (80,86,1), (93,99,1)]
 cCounter = 0
 allChannels = []
+posX = startingPositions[1][0]
+print(posX)
 for counter in range(16,22, 1): # should be the rows that i am getting the channels from 
     cCounter += 1
     channelNums = []
        
     for k, v in racksSheet.items():
-        print(f"{counter}\n")
+        print(f"counter value is: {counter}, k value is: {k}\n")
         channelNums.append(racksSheet[k][cCounter])
-        # channelNumsSliced = channelNums[5:]
+        print(channelNums)
+        
     allChannels.append(channelNums[5:]) # creating a large list for the entire sheet to work from later
     print(channelNums[5:]) # this is where the slice happens and is working 
 print(f"allchannels = {allChannels}")         
