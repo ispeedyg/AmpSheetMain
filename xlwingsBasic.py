0f6a2c6e8aba31c91f80f01a32baba64cb135307
SpeedyG <speedy@ispeedyg.com>
2023-09-26 15:59:37 -0700

    K getting close, now to cut out some fat
    and get it down to getting all the rows of the first section in allChannels list.

--- xlwingsBasic.py
+++ xlwingsBasic.py
@@ -8,32 +8,57 @@ import xlwings as xw
 import pandas as pd  
 path = "/Users/speedy/Documents/GitProjects/AmpSheetRepository/AmpSheetMain/test2.xlsx" 
 wbTest = xw.Book("test2.xlsx")
+data = pd.read_csv("./nba.csv")
 
 ws1 = wbTest.sheets(0)
 # print(wbTest.range('A1).options(numbers=int).value) PRINTS AN INTEGER USE FOR CHANNEL NUMBERS
 racksSheet = pd.read_excel(path, 'Racks')
-wattsSheet = pd.read_excel(path, 'Watts').to_dict()
 
+wattsSheet = pd.read_excel(path, 'Watts').to_dict()
+channelNums = []
 startingPositions = [(3,9,1), (16,22,1), (28,34,1), (41,47,1), (54,60,1), (67,73,1), (80,86,1), (93,99,1)]
+channelNums4 = []
 cCounter = 0
 allChannels = []
 posX = startingPositions[1][0]
 print(posX)
-for counter in range(16,22, 1): # should be the rows that i am getting the channels from 
-    cCounter += 1
-    channelNums = []
-       
-    for k, v in racksSheet.items():
-        print(f"counter value is: {counter}, k value is: {k}\n")
-        channelNums.append(racksSheet[k][cCounter])
-        print(channelNums)
+
+for k, v in racksSheet.items():
+    pass
+row1 = racksSheet.loc[4]
+row2 = data.iloc[3]
+row3 = racksSheet.iloc[4]
+print(row1)
+print('***************************************************')
+for k, v in row3.items():
+    print(v)
+    allChannels.append(v)
+    print('***************************************************')
+print(f"all channels : {allChannels[5:]}")
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+#     for m in range(4, 9, 1):
+#         channelNums4.append(racksSheet.loc[k, m])
+#         fourthSection = channelNums4[:5]
         
-    allChannels.append(channelNums[5:]) # creating a large list for the entire sheet to work from later
-    print(channelNums[5:]) # this is where the slice happens and is working 
-print(f"allchannels = {allChannels}")         
+#     allChannels.append(channelNums[5:]) # creating a large list for the entire sheet to work from later
+#     print(channelNums[5:]) # this is where the slice happens and is working 
+# print(f"allchannels = {allChannels}")         
             
             
-    #         channelNums.append(racksSheet[67][m])
-    #     allChannels.append(channelNums[5:]) # creating a large list for the entire sheet to work from later
-    #     print(channelNums[5:]) # this is where the slice happens and is working 
-    # print(f"all channels = {allChannels}")
+#     #         channelNums.append(racksSheet[67][m])
+#     #     allChannels.append(channelNums[5:]) # creating a large list for the entire sheet to work from later
+#     #     print(channelNums[5:]) # this is where the slice happens and is working 
+#     # print(f"all channels = {allChannels}")
