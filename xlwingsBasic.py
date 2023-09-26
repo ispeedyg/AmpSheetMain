9df58f3db188319752abc51235e21b8e0e66f969
SpeedyG <speedy@ispeedyg.com>
2023-09-25 17:39:59 -0700

    Got this working so far
    It is grabbing all the channels, think i can keep going past 21. Going to try then need to make a class to calculate the amps.

--- xlwingsBasic.py
+++ xlwingsBasic.py
@@ -9,7 +9,15 @@ racksSheet = pd.read_excel(path, 'Racks')
 wattsSheet = pd.read_excel(path, 'Watts').to_dict()
 actualWatts = []
 rowWatts = 0 # to factor that rows total watts
-MAXCHANS = 20
+MAXCHANS = 25 # make it one more so less than is easier plus have to slice 5 off the front
+cCounter = 0
 
-for k, v in racksSheet.items():
-    print(k, v)
+for counter in range(3, 9, 1): # should be the rows that i am getting the channels from 
+    cCounter += 1
+    channelNums = []
+    
+    
+    for k, v in racksSheet.items():
+        
+        channelNums.append(racksSheet[k][cCounter])
+    print(channelNums[5:]) # this is where the slice happens and is working 
