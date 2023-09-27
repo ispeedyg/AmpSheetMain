c5ecc8e1a1ec090172051ecef4e7b20b68925965
SpeedyG <speedy@ispeedyg.com>
2023-09-27 10:26:50 -0700

    Going to start seperating and creating classes

--- xlwingsBasic.py
+++ xlwingsBasic.py
@@ -1,64 +1,27 @@
-import os
 
-def clearScreen():
-    os.system('clear')
-clearScreen()
-
-import xlwings as xw 
 import pandas as pd  
 path = "/Users/speedy/Documents/GitProjects/AmpSheetRepository/AmpSheetMain/test2.xlsx" 
-wbTest = xw.Book("test2.xlsx")
-data = pd.read_csv("./nba.csv")
 
-ws1 = wbTest.sheets(0)
 # print(wbTest.range('A1).options(numbers=int).value) PRINTS AN INTEGER USE FOR CHANNEL NUMBERS
 racksSheet = pd.read_excel(path, 'Racks')
-
-wattsSheet = pd.read_excel(path, 'Watts').to_dict()
-
+# wattsSheet = pd.read_excel(path, 'Watts').to_dict()
+# Below gets all the channels even if there is more than 5
 startingPositions = [1,2,3,4,5,6,14,15,16,17,18,19,26,27,28,29,30,31,39,40,41,42,43,44,52, 53, 54, 55, 56, 57,65,66,67,68,69,70,78,79,80,81,82,83,91,92,93,94,95,96]
 
-cCounter = 0
 allChannels = []
 for position in startingPositions:
-    channelNums = []
-    
-    row1 = racksSheet.loc[position]
+    channelNums = []   # resets channel nums back to blank for next line. 
+    row1 = racksSheet.loc[position + 0] # TODO maybe try a range loop plus (+) the range to the position so for x in range(1,7,1) position + x then reset x
 
-    print('******************** Starting a new ************************')
-    print('***************************************************')
+    # print('******************** Starting a new ************************')
+    # print('***************************************************')
     for k, v in row1.items():
-        print(v)
-        print('---------------------------------------------')
+        # print(v)
+        # print('---------------------------------------------')
         channelNums.append(v)
-        print(f"channelNums: {channelNums}")
-    allChannels.append(channelNums[5:])
-    print('***************************************************')
-    print(f"all channels : {allChannels}")
-
-    
-
-    
+        # print(f"channelNums: {channelNums}")
+    allChannels.append(channelNums[5:]) # slices off the unneeded stuff leaves just the channels
+print('***************************************************')
+print(f"all channels : {allChannels}")
 
 
-
-
-
-
-
-
-
-
-#     for m in range(4, 9, 1):
-#         channelNums4.append(racksSheet.loc[k, m])
-#         fourthSection = channelNums4[:5]
-        
-#     allChannels.append(channelNums[5:]) # creating a large list for the entire sheet to work from later
-#     print(channelNums[5:]) # this is where the slice happens and is working 
-# print(f"allchannels = {allChannels}")         
-            
-            
-#     #         channelNums.append(racksSheet[67][m])
-#     #     allChannels.append(channelNums[5:]) # creating a large list for the entire sheet to work from later
-#     #     print(channelNums[5:]) # this is where the slice happens and is working 
-#     # print(f"all channels = {allChannels}")
