bce59261548aeef095e22ac38df23ed4e46dfd43
SpeedyG <speedy@ispeedyg.com>
2023-09-28 11:02:20 -0700

    We are getting very close here
    I'm in there is printing at the correct places.

--- getWatts.py
+++ getWatts.py
@@ -13,23 +13,15 @@ class Watts:
      
     def whatsWatts(self, path, list):
         data = pd.read_excel(path, 'Watts').to_dict()
-        for subList in range(len(list)):
-            self.temp.append(list[subList])
-    
+        df = pd.DataFrame(list)
+        print(df)
+        for a in range(48):           
+            for x in range(len(data['ChanLow'])): # for in case this length changes with inputs
+                print(f"this is chanLow = {data['ChanLow'][x]}, and chanHi = {data['ChanHigh'][x]}")
+                chanlo = data['ChanLow'][x]
+                chanHi = data['ChanHigh'][x]
         
-        # print(data)
-        # print("****************** Just Checking **********************")
-        # print(list)
-        # print(len(data['ChanLow'])) GETS THE LENGTH OF THE DICT
-        # print(len(list)) GETS THE LENGTH OF THE LIST
-        # print(len(list[0]))
-        for a in range(len(list)):
-            for b in range(len(list[a])):
-                    for x in range(len(data['ChanLow'])): # for in case this length changes with inputs
-                        # print(f"this is chanLow = {data['ChanLow'][x]}, and chanHi = {data['ChanHigh'][x]}")
-                        chanlo = data['ChanLow'][x]
-                        chanHi = data['ChanHigh'][x]
-                        self.counter += 1
-                        # print(f"This is the list of : {list[b]} YEA BABY counter: {self.counter} chanlo = {chanlo} and CHANHI = {chanHi} COOL")
-                        
+                for b in range(8):
+                    if df.loc[a][b] in range(chanlo, chanHi):
+                        print(f"im in there: {df.loc[a][b]}")
             
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from getChans import Channels
 from getWatts import Watts
+ 
+
 newList = []
 watts = Watts()
 channels = Channels()
 allChans = channels.allChannels
 pathTo = channels.path
 channels.getChans(allChans)
-newList = [subList[0] for subList in allChans]
-print(newList)
+
 watts.whatsWatts(path=pathTo, list=allChans)
 
