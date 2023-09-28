1d7c016d4625dea185af30065e3db25b1d61d1ac
SpeedyG <speedy@ispeedyg.com>
2023-09-28 09:57:24 -0700

    still fucking with the list
    going to try and convert to a dictionary to see next

--- getWatts.py
+++ getWatts.py
@@ -8,9 +8,15 @@ class Watts:
         self.lineWatts = 0
         self.totalLineWatts = []
         self.counter = 0
+        self.temp = []
+        
      
     def whatsWatts(self, path, list):
         data = pd.read_excel(path, 'Watts').to_dict()
+        for subList in range(len(list)):
+            self.temp.append(list[subList])
+    
+        
         # print(data)
         # print("****************** Just Checking **********************")
         # print(list)
@@ -20,10 +26,10 @@ class Watts:
         for a in range(len(list)):
             for b in range(len(list[a])):
                     for x in range(len(data['ChanLow'])): # for in case this length changes with inputs
-                    # print(f"this is chanLow = {data['ChanLow'][x]}, and chanHi = {data['ChanHigh'][x]}")
-                    chanlo = data['ChanLow'][x]
-                    chanHi = data['ChanHigh'][x]
-                    self.counter += 1
-                    print(f"This is the list of : {list[a]} YEA BABY counter: {self.counter} chanlo = {chanlo} and CHANHI = {chanHi} COOL")
+                        # print(f"this is chanLow = {data['ChanLow'][x]}, and chanHi = {data['ChanHigh'][x]}")
+                        chanlo = data['ChanLow'][x]
+                        chanHi = data['ChanHigh'][x]
+                        self.counter += 1
+                        # print(f"This is the list of : {list[b]} YEA BABY counter: {self.counter} chanlo = {chanlo} and CHANHI = {chanHi} COOL")
                         
             
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from getChans import Channels
 from getWatts import Watts
-
+newList = []
 watts = Watts()
 channels = Channels()
 allChans = channels.allChannels
 pathTo = channels.path
 channels.getChans(allChans)
-
+newList = [subList[0] for subList in allChans]
+print(newList)
 watts.whatsWatts(path=pathTo, list=allChans)
 
-# have to pass the path and then the allChans to getWatts
