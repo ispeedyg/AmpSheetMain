6a52ece4e474a4f9ed45545ad7a31ad64dfda6fc
SpeedyG <speedy@ispeedyg.com>
2023-09-30 05:47:30 -0700

    Finally got the number without a return
    i need to understand return better. But might have to play with main to get some understanding of the list. Think i will have to dataframe it. But not sure. All this to find an original list for the end when I place amps in correct spots along with 0 for spares. and i have not completed figuring out the lineWatts of a circuit

--- getChans.py
+++ getChans.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 STARTING_POSITIONS = [1,2,3,4,5,6,14,15,16,17,18,19,26,27,28,29,30,31,39,40,41,42,43,44,52, 53, 54, 55, 56, 57,65,66,67,68,69,70,78,79,80,81,82,83,91,92,93,94,95,96]
 
+
+
 class Channels:
     def __init__(self) -> None:
         self.allChannels = []
         self.channelNums = []
+        self.rowLength = int
         self.path = "/Users/speedy/Documents/GitProjects/AmpSheetRepository/AmpSheetMain/test2.xlsx"
         self.racksSheet = pd.read_excel(self.path, 'Racks')
         
@@ -13,14 +16,14 @@ class Channels:
         for position in STARTING_POSITIONS:
             self.channelNums = []   # resets channel nums back to blank for next line. 
             row1 = self.racksSheet.loc[position] # Getting the row to append to AllChannels This will get all 48 circuits
-            rowLength = len(row1) # gives me the length of the row to calculate spare circuitsn later
-            print(f"The row length is : {rowLength - 5}\n") # -5 is the 5 slice away that are not channels
+            self.rowLength = (len(row1)-5) # gives me the length of the row to calculate spare circuitsn later         
+            # -5 is the 5 slice away that are not channels
             for k, v in row1.items():
                
                 self.channelNums.append(v)
                 
             self.allChannels.append(self.channelNums[5:]) # slices off the unneeded stuff leaves just the channels
-        print(self.allChannels)
+        # print(self.allChannels)
         return self.allChannels # returns to main the channels of the sheet
     
     
--- main.py
+++ main.py
@@ -8,8 +8,12 @@ watts = Watts()
 channels = Channels()
 totalWatts = TotalWatts()
 allChans = channels.allChannels
+
 pathTo = channels.path
 channels.getChans(allChans)
+rowLength = channels.rowLength
+print(f"Original list of channels: {allChans}\n")
+print(f"The highest rowlength is : {rowLength}\n")
 daWatts = watts.temp
 getTotalWatts = totalWatts.totalWattsPerLine
 
